734258243209bde2c2288b242f932f48522cf7c7
Ishan <ishanm6@gmail.com>
2017-07-07 23:57:59 +0530

    Assignment2.1
    Largest

---
+++ Assignment2.1/src/Largesst.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+class Largest
+{
+   public static void main(String args[])
+   {
+      int a, b, c;
+      System.out.println("Enter three integers ");
+      Scanner in = new Scanner(System.in);
+ 
+      a = in.nextInt();
+      b = in.nextInt();
+      c = in.nextInt();
+ 
+      if ( a > b && a > c )
+         System.out.println("First number is largest.");
+      else if ( b > a && b > c )
+         System.out.println("Second number is largest.");
+      else if ( c > a && c > b )
+         System.out.println("Third number is largest.");
+      else   
+         System.out.println("Entered numbers are not distinct.");
+   }
+}
