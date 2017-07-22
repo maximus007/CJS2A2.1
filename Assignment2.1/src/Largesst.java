a71453bad7f8266673600c7956e4ec12c1458752
Ishan <ishanm6@gmail.com>
2017-07-22 22:39:00 +0530

    Final
    Commit

--- Assignment2.1/src/Largesst.java
+++ Assignment2.1/src/Largesst.java
@@ -4,17 +4,17 @@ class Largest
 {
    public static void main(String args[])
    {
-      int a, b, c;
-      System.out.println("Enter three integers ");
-      Scanner in = new Scanner(System.in);
+      int a, b, c; // Decalaring variables//
+      System.out.println("Enter three integers "); 
+      Scanner in = new Scanner(System.in); // Scanner class for user input//
  
       a = in.nextInt();
       b = in.nextInt();
       c = in.nextInt();
  
-      if ( a > b && a > c )
-         System.out.println("First number is largest.");
-      else if ( b > a && b > c )
+      if ( a > b && a > c ) // If condition and logical and operator to verify the condition//
+         System.out.println("First number is largest.");// If the statement is true then print this//
+      else if ( b > a && b > c )//Else if condition//
          System.out.println("Second number is largest.");
       else if ( c > a && c > b )
          System.out.println("Third number is largest.");
